996f55a0dcf1c53237d6b23223f17771f3b3f18a
akashyadav1119 <126336516+akashyadav1119@users.noreply.github.com>
2023-09-26 22:38:22 +0530

    Add files via upload

---
+++ mountain_array_peak_value.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int mountain(int arr[], int size)
+{
+    int start = 0, end = size - 1;
+    int mid = start + (end - start) / 2;
+    while (start < end)
+    {
+        if (arr[mid] < arr[mid + 1])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return start;
+}
+int main()
+{
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << mountain(arr, size);
+    return 0;
+}
